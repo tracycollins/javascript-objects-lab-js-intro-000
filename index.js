453172d0ca80a613325a762c59caa8d96b98df82
Tracy Collins <tc@threeceemedia.com>
2020-02-27 05:54:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function updateObjectWithKeyAndValue(obj, key, val) {
-  var newObj = Object.assign({}, obj, {[key]: val})
-  return newObj
+  return Object.assign({}, obj, {[key]: val})
+  // return newObj
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, val) {
+  return
 }
